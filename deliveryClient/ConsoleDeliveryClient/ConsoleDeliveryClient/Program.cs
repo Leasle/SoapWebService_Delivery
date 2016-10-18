3fb9cd9a1416edb55f09513559cf33ec04a558ca
Sergey <sergeeys96@gmail.com>
2016-10-18 22:55:33 +0300

    Add server and client

---
+++ deliveryClient/ConsoleDeliveryClient/ConsoleDeliveryClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDeliveryClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ deliveryClient/WindowsFormsDeliveryClient/WindowsFormsDeliveryClient/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace WindowsFormsDeliveryClient
+{
+    partial class loaderrbt
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameClienttb = new System.Windows.Forms.TextBox();
+            this.phoneNumbertb = new System.Windows.Forms.TextBox();
+            this.datetb = new System.Windows.Forms.TextBox();
+            this.addresstb = new System.Windows.Forms.TextBox();
+            this.nameItem1tb = new System.Windows.Forms.TextBox();
+            this.length1tb = new System.Windows.Forms.TextBox();
+            this.width1tb = new System.Windows.Forms.TextBox();
+            this.height1tb = new System.Windows.Forms.TextBox();
+            this.weight1tb = new System.Windows.Forms.TextBox();
+            this.nameItem2tb = new System.Windows.Forms.TextBox();
+            this.length2tb = new System.Windows.Forms.TextBox();
+            this.width2tb = new System.Windows.Forms.TextBox();
+            this.height2tb = new System.Windows.Forms.TextBox();
+            this.weight2tb = new System.Windows.Forms.TextBox();
+            this.loader = new System.Windows.Forms.RadioButton();
+            this.nameClient = new System.Windows.Forms.Label();
+            this.phoneNumber = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.Label();
+            this.nameItem = new System.Windows.Forms.Label();
+            this.length = new System.Windows.Forms.Label();
+            this.width = new System.Windows.Forms.Label();
+            this.height = new System.Windows.Forms.Label();
+            this.weight = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.orderbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameClienttb
+            // 
+            this.nameClienttb.Location = new System.Drawing.Point(119, 22);
+            this.nameClienttb.Name = "nameClienttb";
+            this.nameClienttb.Size = new System.Drawing.Size(160, 20);
+            this.nameClienttb.TabIndex = 0;
+            // 
+            // phoneNumbertb
+            // 
+            this.phoneNumbertb.Location = new System.Drawing.Point(119, 48);
+            this.phoneNumbertb.Name = "phoneNumbertb";
+            this.phoneNumbertb.Size = new System.Drawing.Size(160, 20);
+            this.phoneNumbertb.TabIndex = 1;
+            // 
+            // datetb
+            // 
+            this.datetb.Location = new System.Drawing.Point(385, 22);
+            this.datetb.Name = "datetb";
+            this.datetb.Size = new System.Drawing.Size(160, 20);
+            this.datetb.TabIndex = 3;
+            // 
+            // addresstb
+            // 
+            this.addresstb.Location = new System.Drawing.Point(385, 48);
+            this.addresstb.Name = "addresstb";
+            this.addresstb.Size = new System.Drawing.Size(160, 20);
+            this.addresstb.TabIndex = 4;
+            // 
+            // nameItem1tb
+            // 
+            this.nameItem1tb.Location = new System.Drawing.Point(119, 168);
+            this.nameItem1tb.Name = "nameItem1tb";
+            this.nameItem1tb.Size = new System.Drawing.Size(160, 20);
+            this.nameItem1tb.TabIndex = 5;
+            // 
+            // length1tb
+            // 
+            this.length1tb.Location = new System.Drawing.Point(119, 194);
+            this.length1tb.Name = "length1tb";
+            this.length1tb.Size = new System.Drawing.Size(160, 20);
+            this.length1tb.TabIndex = 6;
+            // 
+            // width1tb
+            // 
+            this.width1tb.Location = new System.Drawing.Point(119, 220);
+            this.width1tb.Name = "width1tb";
+            this.width1tb.Size = new System.Drawing.Size(160, 20);
+            this.width1tb.TabIndex = 7;
+            // 
+            // height1tb
+            // 
+            this.height1tb.Location = new System.Drawing.Point(119, 246);
+            this.height1tb.Name = "height1tb";
+            this.height1tb.Size = new System.Drawing.Size(160, 20);
+            this.height1tb.TabIndex = 8;
+            // 
+            // weight1tb
+            // 
+            this.weight1tb.Location = new System.Drawing.Point(119, 272);
+            this.weight1tb.Name = "weight1tb";
+            this.weight1tb.Size = new System.Drawing.Size(160, 20);
+            this.weight1tb.TabIndex = 9;
+            // 
+            // nameItem2tb
+            // 
+            this.nameItem2tb.Location = new System.Drawing.Point(330, 168);
+            this.nameItem2tb.Name = "nameItem2tb";
+            this.nameItem2tb.Size = new System.Drawing.Size(160, 20);
+            this.nameItem2tb.TabIndex = 10;
+            // 
+            // length2tb
+            // 
+            this.length2tb.Location = new System.Drawing.Point(330, 194);
+            this.length2tb.Name = "length2tb";
+            this.length2tb.Size = new System.Drawing.Size(160, 20);
+            this.length2tb.TabIndex = 11;
+            // 
+            // width2tb
+            // 
+            this.width2tb.Location = new System.Drawing.Point(330, 220);
+            this.width2tb.Name = "width2tb";
+            this.width2tb.Size = new System.Drawing.Size(160, 20);
+            this.width2tb.TabIndex = 12;
+            // 
+            // height2tb
+            // 
+            this.height2tb.Location = new System.Drawing.Point(330, 246);
+            this.height2tb.Name = "height2tb";
+            this.height2tb.Size = new System.Drawing.Size(160, 20);
+            this.height2tb.TabIndex = 13;
+            // 
+            // weight2tb
+            // 
+            this.weight2tb.Location = new System.Drawing.Point(330, 272);
+            this.weight2tb.Name = "weight2tb";
+            this.weight2tb.Size = new System.Drawing.Size(160, 20);
+            this.weight2tb.TabIndex = 14;
+            // 
+            // loader
+            // 
+            this.loader.AutoSize = true;
+            this.loader.Location = new System.Drawing.Point(385, 77);
+            this.loader.Name = "loader";
+            this.loader.Size = new System.Drawing.Size(58, 17);
+            this.loader.TabIndex = 16;
+            this.loader.TabStop = true;
+            this.loader.Text = "Loader";
+            this.loader.UseVisualStyleBackColor = true;
+            // 
+            // nameClient
+            // 
+            this.nameClient.AutoSize = true;
+            this.nameClient.Location = new System.Drawing.Point(39, 28);
+            this.nameClient.Name = "nameClient";
+            this.nameClient.Size = new System.Drawing.Size(35, 13);
+            this.nameClient.TabIndex = 17;
+            this.nameClient.Text = "Name";
+            // 
+            // phoneNumber
+            // 
+            this.phoneNumber.AutoSize = true;
+            this.phoneNumber.Location = new System.Drawing.Point(39, 51);
+            this.phoneNumber.Name = "phoneNumber";
+            this.phoneNumber.Size = new System.Drawing.Size(76, 13);
+            this.phoneNumber.TabIndex = 18;
+            this.phoneNumber.Text = "Phone number";
+            // 
+            // date
+            // 
+            this.date.AutoSize = true;
+            this.date.Location = new System.Drawing.Point(327, 25);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(30, 13);
+            this.date.TabIndex = 20;
+            this.date.Text = "Date";
+            // 
+            // address
+            // 
+            this.address.AutoSize = true;
+            this.address.Location = new System.Drawing.Point(327, 55);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(45, 13);
+            this.address.TabIndex = 21;
+            this.address.Text = "Address";
+            // 
+            // nameItem
+            // 
+            this.nameItem.AutoSize = true;
+            this.nameItem.Location = new System.Drawing.Point(39, 175);
+            this.nameItem.Name = "nameItem";
+            this.nameItem.Size = new System.Drawing.Size(57, 13);
+            this.nameItem.TabIndex = 22;
+            this.nameItem.Text = "Name item";
+            // 
+            // length
+            // 
+            this.length.AutoSize = true;
+            this.length.Location = new System.Drawing.Point(39, 201);
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(40, 13);
+            this.length.TabIndex = 23;
+            this.length.Text = "Length";
+            // 
+            // width
+            // 
+            this.width.AutoSize = true;
+            this.width.Location = new System.Drawing.Point(39, 227);
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(35, 13);
+            this.width.TabIndex = 24;
+            this.width.Text = "Width";
+            // 
+            // height
+            // 
+            this.height.AutoSize = true;
+            this.height.Location = new System.Drawing.Point(39, 253);
+            this.height.Name = "height";
+            this.height.Size = new System.Drawing.Size(38, 13);
+            this.height.TabIndex = 25;
+            this.height.Text = "Height";
+            // 
+            // weight
+            // 
+            this.weight.AutoSize = true;
+            this.weight.Location = new System.Drawing.Point(39, 275);
+            this.weight.Name = "weight";
+            this.weight.Size = new System.Drawing.Size(41, 13);
+            this.weight.TabIndex = 26;
+            this.weight.Text = "Weight";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(177, 6);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(33, 13);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Client";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(449, 6);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(33, 13);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "Order";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(177, 142);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(36, 13);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "Item 1";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(391, 142);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(36, 13);
+            this.label14.TabIndex = 30;
+            this.label14.Text = "Item 2";
+            // 
+            // orderbtn
+            // 
+            this.orderbtn.Location = new System.Drawing.Point(514, 270);
+            this.orderbtn.Name = "orderbtn";
+            this.orderbtn.Size = new System.Drawing.Size(75, 23);
+            this.orderbtn.TabIndex = 31;
+            this.orderbtn.Text = "Add order";
+            this.orderbtn.UseVisualStyleBackColor = true;
+            this.orderbtn.Click += new System.EventHandler(this.orderbtn_Click);
+            // 
+            // loaderrbt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 319);
+            this.Controls.Add(this.orderbtn);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.weight);
+            this.Controls.Add(this.height);
+            this.Controls.Add(this.width);
+            this.Controls.Add(this.length);
+            this.Controls.Add(this.nameItem);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.phoneNumber);
+            this.Controls.Add(this.nameClient);
+            this.Controls.Add(this.loader);
+            this.Controls.Add(this.weight2tb);
+            this.Controls.Add(this.height2tb);
+            this.Controls.Add(this.width2tb);
+            this.Controls.Add(this.length2tb);
+            this.Controls.Add(this.nameItem2tb);
+            this.Controls.Add(this.weight1tb);
+            this.Controls.Add(this.height1tb);
+            this.Controls.Add(this.width1tb);
+            this.Controls.Add(this.length1tb);
+            this.Controls.Add(this.nameItem1tb);
+            this.Controls.Add(this.addresstb);
+            this.Controls.Add(this.datetb);
+            this.Controls.Add(this.phoneNumbertb);
+            this.Controls.Add(this.nameClienttb);
+            this.Name = "loaderrbt";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameClienttb;
+        private System.Windows.Forms.TextBox phoneNumbertb;
+        private System.Windows.Forms.TextBox datetb;
+        private System.Windows.Forms.TextBox addresstb;
+        private System.Windows.Forms.TextBox nameItem1tb;
+        private System.Windows.Forms.TextBox length1tb;
+        private System.Windows.Forms.TextBox width1tb;
+        private System.Windows.Forms.TextBox height1tb;
+        private System.Windows.Forms.TextBox weight1tb;
+        private System.Windows.Forms.TextBox nameItem2tb;
+        private System.Windows.Forms.TextBox length2tb;
+        private System.Windows.Forms.TextBox width2tb;
+        private System.Windows.Forms.TextBox height2tb;
+        private System.Windows.Forms.TextBox weight2tb;
+        private System.Windows.Forms.RadioButton loader;
+        private System.Windows.Forms.Label nameClient;
+        private System.Windows.Forms.Label phoneNumber;
+        private System.Windows.Forms.Label date;
+        private System.Windows.Forms.Label address;
+        private System.Windows.Forms.Label nameItem;
+        private System.Windows.Forms.Label length;
+        private System.Windows.Forms.Label width;
+        private System.Windows.Forms.Label height;
+        private System.Windows.Forms.Label weight;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button orderbtn;
+    }
+}
+
